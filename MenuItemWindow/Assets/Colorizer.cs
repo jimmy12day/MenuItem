491bbfd834b938cacc6d077df49875980f0faad4
Bin <jimmy28year@gmail.com>
2017-12-19 16:19:27 -0800

    Create a menuItem demo project
    Can Select multiple gameobject and assign the color by - Colorizer/Colorizer/Colorizier Button

---
+++ MenuItemWindow/Assets/Colorizer.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEditor;
+using UnityEngine;
+
+public class Colorizer : EditorWindow
+{
+    Color color;
+    // Add menu named "My Window" to the Window menu
+    [MenuItem("Colorizer/Colorizer")]
+    public static void ShowWindow()
+    {
+        GetWindow<Colorizer>("Colorizer");
+    }
+
+    private void OnGUI()
+    {
+
+        color = EditorGUILayout.ColorField("Color", color);
+        if (GUILayout.Button("Colorizer"))
+        {
+            foreach (GameObject obj in Selection.gameObjects)
+            {
+                Renderer renderer = obj.GetComponent<Renderer>();
+                if (renderer)
+                {
+                    renderer.sharedMaterial.color = color;
+                }
+            }
+        }
+    }
+}
+
+
